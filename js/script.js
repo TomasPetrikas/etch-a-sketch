5a6e4f4cdfca21cbac4cc4bcbd2c904bb457b3bf
Tomas Petrikas <tomas.petrikas99@gmail.com>
2021-12-30 11:56:31 +0200

    Add basic draw capability

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
-grid = document.querySelector("#container");
+// Generates a square grid of a given size
+function generateGrid(size) {
+  let grid = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-  let col = document.createElement("div");
-  col.classList = "column";
-
-  for (let j = 0; j < 16; j++) {
-    let row = document.createElement("div");
-    row.classList = "square";
-    col.appendChild(row);
+  for (let i = 0; i < size; i++) {
+    let col = document.createElement("div");
+    col.classList = "column";
+  
+    for (let j = 0; j < size; j++) {
+      let row = document.createElement("div");
+      row.classList = "square";
+      col.appendChild(row);
+    }
+    grid.appendChild(col);
   }
-  grid.appendChild(col);
 }
+
+function changeBackground(e) {
+  const red = Math.random() * 255;
+  const green = Math.random() * 255;
+  const blue = Math.random() * 255;
+  e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+}
+
+generateGrid(16);
+
+const squares = [...document.querySelectorAll(".square")];
+squares.forEach(function(square) {
+  square.addEventListener("mouseover", changeBackground)
+});
